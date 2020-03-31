9455a4b605fa58cb3a14d1405b2193556797b48f
王瑞华 <wangruihua@wangruihuadeMacBook-Pro.local>
2020-03-31 10:01:38 +0800

    update config.py

--- zabbix-police/config.py
+++ zabbix-police/config.py
@@ -2,25 +2,25 @@
 
 #------ zabbix 配置--------------------------------------------
 #zabbix数据库IP                                                
-zabbix_db_host = "rm-bp1k3597mn06850z9.mysql.rds.aliyuncs.com"
+zabbix_db_host = ""
 #zabbix数据库端口                                              
 zabbix_db_port = 3306 
 #zabbix数据库名称                                       
-zabbix_db_name = "zabbix" 
+zabbix_db_name = "" 
 #zabbix数据库登录用户名                                   
-zabbix_db_username = "zabbix"
+zabbix_db_username = ""
 #zabbix数据库登录密码                             
-zabbix_db_password = "hitales@2018" 
+zabbix_db_password = "" 
 #在zabbix 可以找到告警收敛的动作ID（actionid）                 
 actionid = 8                                            
 #图片保存地址
 image_path = "/data/zabbix_sendimg/"
 #zabbix host
-zabbix_host = "http://monitor.hitales.ai:8080/h-monitor"
+zabbix_host = ""
 #zabbix web 登录用户名
 zabbix_username = "admin"
 #zabbix web 登录密码
-zabbix_password = "Hitales@2018"
+zabbix_password = ""
 #--------------------------------------------------------------
 
 #------ redis 配置----------------
@@ -34,15 +34,15 @@ redis_port = 6379
 #发送微信给运维人员 
 Users = '@all'
 #企业微信CropID                                         
-CorpID = 'wwae5296a1215aee9b' 
+CorpID = '' 
 #企业微信告警群1  Secret                            
-Secret_1 = 'SOAjIbkJFJdZ7iltK7qKsFbjCE3jLcZ3Hq79qwc0yQo' 
+Secret_1 = '' 
 #企业微信告警群1 agentid    
 Agentid_1 = "1000012" 
 
 #专门发错误日志的告警群
 #企业微信告警群2  Secret                            
-Secret_2 = '7dvvTfKXlaWezaj9gMJs89TPmZyiVHp3T_Jaehx6ROI' 
+Secret_2 = '' 
 #企业微信告警群2 agentid    
 Agentid_2 = "1000019" 
 #--------------------------------------------------------
